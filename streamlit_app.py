484c2b944eebeeab1cf905e166dc0d762f20b2bb
smiling-cheetah <karandinii@gmail.com>
2022-04-23 11:07:12 +0300

    Update streamlit_app.py
    Prepopulate selections in table

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #fruit picker
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 #display table on the page
 streamlit.dataframe(my_fruit_list)
