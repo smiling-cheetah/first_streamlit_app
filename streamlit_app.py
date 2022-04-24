3345909ef65a440b3aa427c18fbf3bbe916b3da7
smiling-cheetah <karandinii@gmail.com>
2022-04-24 16:04:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,13 +29,17 @@ streamlit.dataframe(fruits_to_show)
 
 #import data from fruity vice 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered',fruit_choice)
-
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
-#parse json response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+  if not fruit_choice:
+    streamlit.write('Please select a fruit to get information')
+  else:  
+    fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
+    #parse json response
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #dont run anything past here while we troubleshoot
 
